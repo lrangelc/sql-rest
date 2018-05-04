c8de94a145598322a3a600efc54c2f9b54870ca6
Luis Rangel Castro <luisrangelc@rpaipos.com>
2018-05-03 23:55:26 -0600

    fix1

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 var connection = mysql.createConnection({
     host:"localhost"
     ,user:"root"
-    ,password:"adminval"
+    ,password:"XXX"
     ,database:"platzi"
 });
 
